5fbacb7a189ed6813f6653ba55a02b66777ca52a
quentin.tambone <quentin.tambone@universite-paris-saclay.fr>
2022-05-01 18:23:13 -0400

    ajout Cas 1 et 2 Quentin

--- Projet-Inf-1008/src/Registre.java
+++ Projet-Inf-1008/src/Registre.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 
 public class Registre {
 
@@ -28,6 +29,77 @@ public class Registre {
 		
 		GestionnaireClient.CreationClient(nom, prenom, adresse, noTelDom, noTelMob, noPermis, noCarteBancaire);
 		gui.showMessage("Le compte client a été créer avec succès", "Message système");
+		gui.toUpperMenu();
+	}
+
+
+	/** ------------------------------------------------------------------------------------------------------------------**/
+
+
+	/** Méthode creerReservation ayant pour but de créer une réservation.
+	 * @author Quentin Tambone
+	 * @param categorie : une catégorie de véhicule
+	 * @param dateDebut : une date de début de réservation
+	 * @param dateDebut : une date de fin de réservation
+	 * @param gui : GUI du menu upper
+	 */
+	public Reservation creerReservation(String categorie, Date dateDebut, Date dateFin, GUI gui){
+		Reservation resa = Reservation.creerReservation(categorie, dateDebut, dateFin) ;
+		gui.showMessage("La réservation a été créer avec succès", "Message système") ;
+		gui.toUpperMenu();
+		return resa ;
+	}
+
+	/** Méthode associerClientReservation ayant pour but de créer d'associer le client à sa réservation.
+	 * @author Quentin Tambone
+	 * @param ID_Client : un identifiant client
+	 * @param resa : une reservation
+	 * @param gui : GUI du menu upper
+	 */
+	public static void associerClientReservation(int ID_Client, Reservation resa, GUI gui){
+		Client c = GestionnaireClient.getClient(ID_Client);
+		resa.setID_client(ID_Client) ;
+		gui.showMessage("voici le résumé de la reservation : " + "\n" + resa.toString(),"Message système") ;
+		gui.toUpperMenu();
+	}
+
+	/** Méthode confirmationReservation ayant pour but de d'enregistrer un reservation confirmée.
+	 * @author Quentin Tambone
+	 * @param resa : une reservation
+	 * @param g : une gestionnaire de réservation
+	 * @param gui : GUI du menu upper
+	 */
+	public static void confirmationReservation(Reservation resa, GestionnaireReservation g, GUI gui){
+		resa.enregistrerReservation(g) ;
+		gui.showMessage("La réservation a bien été enregistrée", "Message système") ;
+		gui.toUpperMenu();
+	}
+
+	/** Méthode rechercherReservation ayant pour but de trouver une reservation particulière.
+	 * @author Quentin Tambone
+	 * @param numResa : un numéro de reservation
+	 * @param g : une gestionnaire de réservation
+	 * @param gui : GUI du menu upper
+	 */
+	public static Reservation rechercherReservation(Integer numResa, GestionnaireReservation g, GUI gui){
+		Reservation resa = g.getReservation(numResa) ;
+		gui.showMessage("voici la reservation recherchée" + "\n" + resa.toString(), "Message système") ;
+		gui.toUpperMenu();
+		return resa ;
+	}
+
+
+	/** Méthode modificationReservation ayant pour but de modifier une reservation.
+	 * @author Quentin Tambone
+	 * @param resa : une reservation
+	 * @param categorie : une catégorie de véhicule
+	 * @param dateDebut : une date de début de réservation
+	 * @param dateDebut : une date de fin de réservation
+	 * @param gui : GUI du menu upper
+	 */
+	public static void modificationReservation(Reservation resa, String categorie, Date dateDebut, Date dateFin, GUI gui){
+		resa.setAttribut(categorie, dateDebut,dateFin) ;
+		gui.showMessage("voici la reservation modifiée" + "\n" + resa.toString(), "Message système") ;
 		gui.toUpperMenu();
 	}
 
